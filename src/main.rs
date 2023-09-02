e18bee188ad4f257a45b2204707bf1b97ea2aa9a
Filip Pavlovic <filpavlovic@gmail.com>
2023-09-03 00:38:22 +0200

    base skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io::{self, Write};
+
+enum WhatDo {
+    Exit,
+    Fork { name: String, args: Vec<String> },
+    Parallel { parts: Vec<WhatDo> },
+}
+
 fn main() {
-    println!("Hello, world!");
+    print!("> ");
+    io::stdout().flush().expect("flushE");
+    let mut line = String::new();
+    while let Ok(len) = io::stdin().read_line(&mut line) {
+        if len == 0 || line.trim() == "exit"  {
+            break;
+        }
+        print!("> ");
+        io::stdout().flush().expect("flushE");
+        line = String::new();
+    }
 }
