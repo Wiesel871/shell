fe0cb538f6f2ba4873c78698741541b12f6060a1
Filip Pavlovič <xpavlov3@fi.muni.cz>
2023-09-03 19:08:46 +0200

    added option to specify input file instead of strictly stdin

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
-use std::io::{self, Write};
+use std::io::{stdin, stdout, stderr, Write, BufReader, BufRead};
 
 enum WhatDo {
     Exit,
+
     Fork { name: String, args: Vec<String> },
     Parallel { parts: Vec<WhatDo> },
 }
 
-fn main() {
-    print!("> ");
-    io::stdout().flush().expect("flushE");
+fn main() -> std::io::Result<()> {
+    let should_print = |b| {if b {print!("> "); stdout().flush().expect("flushE");}};
+    let basic_commands = vec!["echo", "exit", "pwd", "cd"];
+    let (mut input_file, out): (Box<dyn BufRead>, bool) = match std::env::args().nth(1) {
+        Some(input) => (Box::new(BufReader::new(std::fs::File::open(input).unwrap())), 
+                        false),
+        None => (Box::new(BufReader::new(stdin())),
+                true)
+    };
+    should_print(out);
     let mut line = String::new();
-    while let Ok(len) = io::stdin().read_line(&mut line) {
-        if len == 0 || line.trim() == "exit"  {
+    while let Ok(len) = input_file.read_line(&mut line) {
+        if len == 0 {
             break;
         }
-        print!("> ");
-        io::stdout().flush().expect("flushE");
+
+        should_print(out);
         line = String::new();
-    }
+    };
+    Ok(())
 }
